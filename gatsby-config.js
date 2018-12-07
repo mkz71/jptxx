f18fa31bf6e246e4443ff966f1b6c068c8275cfb
AerialWombat <adrianzhan@gmail.com>
2018-12-06 19:42:52 -0500

    tweaked netlify cms editor config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
                 name: "menu-items"
             }
         },
+        `gatsby-transformer-remark`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-plugin-typography`,
--- src/pages/Menu/menu.js
+++ src/pages/Menu/menu.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../../components/Layout/layout.js";
+import { graphql } from "gatsby";
 
-const Menu = () => (
+const Menu = ({ data }) => (
     <Layout>
         <h1>Menu</h1>
     </Layout>
 )
+
+// export const pageQuery = graphql`
+//     query($path: String!)
 
 export default Menu;

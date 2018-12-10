ee42c1ada03da42845b26b4f47b0ce59117d5e01
AerialWombat <adrianzhan@gmail.com>
2018-12-10 18:28:06 -0500

    added check for item component

--- src/pages/Menu/item.js
+++ src/pages/Menu/item.js
@@ -2,15 +2,15 @@ import React from "react";
 import styles from "./menu.module.css";
 
 const Item = ( props ) => {
-    // const { title, price, smallPrice, description } = props.frontmatter;
+    const { title, price, smallPrice, description } = props.frontmatter;
     return (
     <div className={styles.menuItem}>
         <span className={styles.itemHeader}>
-            <h1 className={styles.itemName}>{props.frontmatter.title}</h1>
-            {props.frontmatter.smallPrice ? <p className={styles.price}>M: {props.frontmatter.smallPrice}</p> : ""}
-            <p className={styles.price}>L: {props.frontmatter.price}</p>
+            <h1 className={styles.itemName}>{title}</h1>
+            {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
+            <p className={styles.price}>L: {price}</p>
         </span>
-        {props.frontmatter.description ? <p className={styles.description}>{props.frontmatter.description}</p> : ""}
+        {description ? <p className={styles.description}>{description}</p> : ""}
     </div>)
 }
 

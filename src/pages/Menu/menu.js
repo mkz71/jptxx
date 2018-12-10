d18bf42c1dea9b2da300835b7776e9286b29fbc7
AerialWombat <adrianzhan@gmail.com>
2018-12-10 16:20:01 -0500

    added menu page

--- src/pages/Menu/menu.js
+++ src/pages/Menu/menu.js
@@ -1,17 +1,61 @@
 import React from "react";
 import Layout from "../../components/Layout/layout.js";
+import styles from "./menu.module.css";
 import { graphql } from "gatsby";
 
-const Menu = ({ data }) => (
-    <Layout>
-        <h1>Menu</h1>
-        <p>{ data.allMarkdownRemark.edges[0].node.frontmatter.title }</p>
-        <p>{ data.allMarkdownRemark.edges[0].node.frontmatter.category }</p>
-        <p>{ data.allMarkdownRemark.edges[0].node.frontmatter.price }</p>
-        <p>{ data.allMarkdownRemark.edges[0].node.frontmatter.smallPrice }</p>
-        <p>{ data.allMarkdownRemark.edges[0].node.frontmatter.description }</p>
-    </Layout>
-)
+const Menu = ({ data }) => {
+    const sections = [
+        { title: "Soup", subheader: "Comes with crispy noodles" },
+        { title: "Appetizer", subheader: "Amounts per order shown" },
+        { title: "House Specialties", subheader: "Served with white rice" },
+        { title: "Seafood", subheader: "Served with white rice" },
+        { title: "Poultry", subheader: "Served with white rice" },
+        { title: "Beef", subheader: "Served with white rice" },
+        { title: "Pork", subheader: "Served with white rice" },
+        { title: "Vegetable", subheader: "Served with white rice" },
+        { title: "Steamed", subheader: "Served with white rice and brown sauce on the side" },
+        { title: "Fried Rice/Chow Mein/Lo Mein", subheader: "Choice of combination, shrimp, chicken, beef, or pork" },
+        { title: "Egg Foo Young", subheader: "Served with white rice and gravy" },
+        { title: "American", subheader: "Served with fried Rice or french fries or steamed broccoli" },
+        { title: "Combo", subheader: "Lunch (11AM - 3PM) served with egg roll, fried rice, & can of soda | Dinner served with egg roll, fried rice, & small soup" }
+    ]
+
+    return (
+        <Layout>
+            {
+                sections.map(section => {
+                    return (
+
+                        <section className={styles.menuSection}>
+                            <header className={styles.sectionHeader}>
+                                <h1 className={styles.sectionTitle}>{section.title}</h1>
+                                <h3 className={styles.sectionSubheader}>{section.subheader}</h3>
+                            </header>
+                            {
+                                data.allMarkdownRemark.edges.map(element => {
+                                    const { title, price, smallPrice, description, category } = element.node.frontmatter;
+                                    if (category === section.title) {
+                                        return (
+                                            <div className={styles.menuItem}>
+                                                <span className={styles.itemHeader}>
+                                                    <h1 className={styles.itemName}>{title}</h1>
+                                                    {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
+                                                    <p className={styles.price}>L: {price}</p>
+                                                </span>
+                                                {description ? <p className={styles.description}>{description}</p> : ""}
+                                            </div>
+                                        )
+                                    }
+                                })
+                            }
+                        </section>
+
+                    )
+                })
+            }
+        </Layout>
+    )
+}
 
 export const query = graphql`
     query menuQuery {
@@ -33,4 +77,5 @@ export const query = graphql`
 `
 export default Menu;
 
-// Try filter or sort graphql query for categories 
+// Use loop to create each section
+// Refactor menu items into components

a7e4f461eca073939a105bb729723e6f2c67659f
AerialWombat <adrianzhan@gmail.com>
2018-12-10 18:10:41 -0500

    added check for item component

--- src/pages/Menu/item.js
+++ src/pages/Menu/item.js
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./menu.module.css";
 
-const Item = ( props ) => {
-    const { title, price, smallPrice, description, category } = props.frontmatter;
-    return (
-    <div className={styles.menuItem}>
-        <span className={styles.itemHeader}>
-            <h1 className={styles.itemName}>{title}</h1>
-            {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
-            <p className={styles.price}>L: {price}</p>
-        </span>
-        {description ? <p className={styles.description}>{description}</p> : ""}
-    </div>)
+const Item = (props) => {
+    if (typeof props.frontmatter !== undefined) {
+        const { title, price, smallPrice, description } = props.frontmatter;
+        return (
+            <div className={styles.menuItem}>
+                <span className={styles.itemHeader}>
+                    <h1 className={styles.itemName}>{title}</h1>
+                    {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
+                    <p className={styles.price}>L: {price}</p>
+                </span>
+                {description ? <p className={styles.description}>{description}</p> : ""}
+            </div>
+        )
+    }
+
 }
 
 export default Item;

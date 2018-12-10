0c86e61798abc0249b2cfdd6dc1d373b64558c6c
AerialWombat <adrianzhan@gmail.com>
2018-12-10 18:12:48 -0500

    added check for item component

--- src/pages/Menu/item.js
+++ src/pages/Menu/item.js
@@ -1,21 +1,17 @@
 import React from "react";
 import styles from "./menu.module.css";
 
-const Item = (props) => {
-    if (typeof props.frontmatter !== undefined) {
-        const { title, price, smallPrice, description } = props.frontmatter;
-        return (
-            <div className={styles.menuItem}>
-                <span className={styles.itemHeader}>
-                    <h1 className={styles.itemName}>{title}</h1>
-                    {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
-                    <p className={styles.price}>L: {price}</p>
-                </span>
-                {description ? <p className={styles.description}>{description}</p> : ""}
-            </div>
-        )
-    }
-
+const Item = ( props ) => {
+    // const { title, price, smallPrice, description } = props.frontmatter;
+    return (
+    <div className={styles.menuItem}>
+        <span className={styles.itemHeader}>
+            <h1 className={styles.itemName}>{props.frontmatter.title}</h1>
+            {props.frontmatter.smallPrice ? <p className={styles.price}>M: {props.frontmatter.smallPrice}</p> : ""}
+            <p className={styles.price}>L: {props.frontmatter.price}</p>
+        </span>
+        {props.frontmatter.description ? <p className={styles.description}>{props.frontmatter.description}</p> : ""}
+    </div>)
 }
 
 export default Item;

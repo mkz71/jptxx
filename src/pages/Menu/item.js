aab82f3fa8b96adf935ee6707c1dbddb2b8e253d
AerialWombat <adrianzhan@gmail.com>
2018-12-10 18:56:54 -0500

    add staticqury to item component

--- src/pages/Menu/item.js
+++ src/pages/Menu/item.js
@@ -1,17 +1,52 @@
 import React from "react";
 import styles from "./menu.module.css";
+import { StaticQuery, graphql } from "gatsby";
 
-const Item = ( props ) => {
-    const { title, price, smallPrice, description } = props.frontmatter;
-    return (
+// const Item = ( props ) => {
+//     console.log(props.frontmatter);
+//     const { title, price, smallPrice, description } = props.frontmatter;
+//     return (
+//     <div className={styles.menuItem}>
+//         <span className={styles.itemHeader}>
+//             <h1 className={styles.itemName}>{title}</h1>
+//             {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
+//             <p className={styles.price}>L: {price}</p>
+//         </span>
+//         {description ? <p className={styles.description}>{description}</p> : ""}
+//     </div>)
+// }
+
+const Item = () => (
+    <StaticQuery
+        query={graphql`
+        query itemQuery {
+            allMarkdownRemark {
+                edges {
+                  node {
+                    frontmatter {
+                      title
+                      path
+                      category
+                      price
+                      smallPrice
+                      description
+                    }
+                  }
+                }
+              }
+        }
+    `}
+        render={data => (
     <div className={styles.menuItem}>
         <span className={styles.itemHeader}>
-            <h1 className={styles.itemName}>{title}</h1>
-            {smallPrice ? <p className={styles.price}>M: {smallPrice}</p> : ""}
-            <p className={styles.price}>L: {price}</p>
+            <h1 className={styles.itemName}>{data.allMarkdownRemark.edges.node.frontmatter.title}</h1>
+            {data.allMarkdownRemark.edges.node.frontmatter.smallPrice ? <p className={styles.price}>M: {data.allMarkdownRemark.edges.node.frontmatter.smallPrice}</p> : ""}
+            <p className={styles.price}>L: {data.allMarkdownRemark.edges.node.frontmatter.price}</p>
         </span>
-        {description ? <p className={styles.description}>{description}</p> : ""}
-    </div>)
-}
+        {data.allMarkdownRemark.edges.node.frontmatter.description ? <p className={styles.description}>{data.allMarkdownRemark.edges.node.frontmatter.description}</p> : ""}
+    </div>
+        )}
+    />
+)
 
 export default Item;

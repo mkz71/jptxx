c9c004ca08587aada86c1dd1ca7140e328f6bf25
AerialWombat <adrianzhan@gmail.com>
2019-03-12 20:25:39 -0400

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,39 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: "menu-items",
-                path: `${__dirname}/menu-items`
-            }
-        },
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography.js`
-            }
-        },
-        `gatsby-plugin-netlify-cms`,
-        `gatsby-plugin-react-helmet`
-    ]
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "menu-items",
+        path: `${__dirname}/menu-items`
+      }
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-136092604-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10
+      }
+    }
+  ]
+};
 
 //gatsby-plugin-sharp, gatsby-transformer-sharp, gatsby-image

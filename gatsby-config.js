423603e73a07b9d02f98bb5421bf4c4321b08a5d
AerialWombat <adrianzhan@gmail.com>
2019-03-14 17:17:57 -0400

    Added Google Analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,18 +19,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-136092604-1"
-        // // Puts tracking script in the head instead of the body
-        // head: false,
-        // // Setting this parameter is optional
-        // anonymize: true,
-        // // Setting this parameter is also optional
-        // respectDNT: true,
-        // // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // // Any additional create only fields (optional)
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10
+        trackingId: "UA-136092604-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10
       }
     }
   ]

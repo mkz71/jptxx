5b4fc40ce56d0e43bb5301e3ac102db0716cbb25
Mongkol Horburapa <m.horburapa@gmail.com>
2019-10-10 14:28:46 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       options: {
         trackingId: "UA-149514152-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
